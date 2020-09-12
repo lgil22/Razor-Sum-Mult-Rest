3287592c18c288ae2fca7f02b54822f7a98738e9
Luis David Gil Baez <luis_gil@ucne.edu.do>
2020-09-12 17:52:15 -0400

    Add project files.

---
+++ Entidades/Algoritmo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculator.Entidades
+{
+    public class Algoritmo
+    {
+        public float Num1 { get; set; }
+        public float Num2 { get; set; }
+        public float ResultadoSuma { get; set; }
+        public float ResultadoResta { get; set; }
+        public float ResultadoMultiplicacion { get; set; }
+
+        
+        public Algoritmo()
+        {
+            Num1 = 0;
+            Num2 = 0;
+            ResultadoSuma = 0;
+            ResultadoResta = 0;
+            ResultadoMultiplicacion = 0;
+        }
+    }
+}
